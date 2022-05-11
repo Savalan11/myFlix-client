34372b9aac6cc05aa988823bd57524a384cd4e0c
Savalan Suleymanli <98057609+Savalan11@users.noreply.github.com>
2022-05-11 22:32:33 +0200

    update of components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,25 @@
+// myFlix-client/src/main-view/main-view.jsx
+
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
-class MainView extends React.Component {
+import { MovieView } from '../movie-view/movie-view';
 
+export class MainView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [{ _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg' },
+            { _id: 2, Title: 'The Shawshank Redemption', Description: 'The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNTYxOTYyMzE3NV5BMl5BanBnXkFtZTcwOTMxNDY3Mw@@._V1_.jpg' },
+            { _id: 3, Title: 'Gladiator', Description: 'Gladiator is a 2000 epic historical drama film directed by Ridley Scott and written by David Franzoni, John Logan, and William Nicholson.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png' }],
+            selectedMovie: null
+        };
+    }
 
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
+    }
     render() {
         const { movies, selectedMovie } = this.state;
 
@@ -22,15 +39,3 @@ class MainView extends React.Component {
 }
 
 export default MainView;
-    /*    render() {
-    return (
-        <div className="main-view">
-            <div>Inception</div>
-            <div>The Shawshank Redemption</div>
-            <div>Gladiator</div>
-        </div>
-    );
-}
-}
-
-export default MainView; */
