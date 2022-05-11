6113508a859f40def54cba3b2ac0ff5948c2b60a
Savalan Suleymanli <98057609+Savalan11@users.noreply.github.com>
2022-05-11 13:11:19 +0200

    creation of folders and files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
-
+import { MovieCard } from '../movie-card/movie-card';
 class MainView extends React.Component {
 
+
     render() {
+        const { movies, selectedMovie } = this.state;
+
+        if (movies.length === 0) return <div className="main-view" />;
+
         return (
             <div className="main-view">
-                <div>Inception</div>
-                <div>The Shawshank Redemption</div>
-                <div>Gladiator</div>
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
+                    ))
+                }
             </div>
         );
     }
 }
 
 export default MainView;
+    /*    render() {
+    return (
+        <div className="main-view">
+            <div>Inception</div>
+            <div>The Shawshank Redemption</div>
+            <div>Gladiator</div>
+        </div>
+    );
+}
+}
+
+export default MainView; */
