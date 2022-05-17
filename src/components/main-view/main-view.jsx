617da5914cd1742120b429efeb0d1a0c55bbbece
Savalan Suleymanli <98057609+Savalan11@users.noreply.github.com>
2022-05-17 11:23:03 +0200

    update files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,18 +1,33 @@
 // myFlix-client/src/main-view/main-view.jsx
 
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+//getting array of movies from remote and displaying as a list
 export class MainView extends React.Component {
     constructor() {
         super();
+        //initial state for main-view
         this.state = {
-            movies: [{ _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg' },
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont, based on the 1982 Stephen King novella Rita Hayworth', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNTYxOTYyMzE3NV5BMl5BanBnXkFtZTcwOTMxNDY3Mw@@._V1_.jpg' },
-            { _id: 3, Title: 'Gladiator', Description: 'Gladiator is a 2000 epic historical drama film directed by Ridley Scott and written by David Franzoni, John Logan, and William Nicholson.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png' }],
-            selectedMovie: null
+            movies: [],
+            selectedMovie: null,
+            registered: null,
+            user: null,
         };
+    }
+    componentDidMount() {
+        axios.get('https://[APP-NAME].herokuapp.com/movies')
+            .then(response => {
+                this.setState({
+                    movies: response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -37,5 +52,3 @@ export class MainView extends React.Component {
         );
     }
 }
-
-export default MainView;
