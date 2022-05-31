df7bc3da37fff99a7c8afd4babbbf7cb81fbd066
Savalan Suleymanli <savalan.suleymanli@gmail.com>
2022-05-31 11:09:49 +0200

    add relevant codes and bootstrap referral

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,27 +1,24 @@
+// myFlix-client/src/components/movie-card/movie-card.jsx
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./movie-card.scss"
 
 export class MovieCard extends React.Component {
-    render() {
-        const { movie, onMovieClick } = this.props;
+  render() {
+    const { movie, onMovieClick } = this.props;
 
-        return (
-            <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
-        );
-    }
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
-
-MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string,
-        }),
-        Director: PropTypes.shape({
-            Name: PropTypes.string,
-        }),
-    }).isRequired,
-
-    onMovieClick: PropTypes.func.isRequired,
-};
